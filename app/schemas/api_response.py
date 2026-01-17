e00627e9c5dae686a6519085c729524fbd4f89e4
Roland Almeda <rolandalmeda@Rolands-MacBook-Air.local>
2026-01-17 19:35:31 +1100

    feature: Adding ListResponse wrapper

--- app/schemas/api_response.py
+++ app/schemas/api_response.py
@@ -3,8 +3,15 @@ from pydantic.alias_generators import to_camel
 
 
 class APIResponse(BaseModel):
+    """
+    Extend the API base schema for use as a GET request response schema.
+
+    Reads data from SQLModel instances and serializes it into SCIM-compliant 
+    JSON for FastAPI responses.
+    """
+
     model_config = {
-        "alias_generator": to_camel, # during serialisation
+        "alias_generator": to_camel, # convert database ORM attributes to camel case to ensure SCIM compliant return (JSON).
         "populate_by_name": True, # populate the python attributes
         "from_attributes": True # read data from SQLModel instances
     }
---
+++ app/schemas/list_response.py
@@ -0,0 +1,6 @@
+
+from app.schemas.api_response import APIResponse
+
+class ListResponse(APIResponse):
+
+    
